31ce6052c6526d819de4204b702b341b67ea62ec
JHNLWHD <aljhoenilw@gmail.com>
2019-12-13 16:59:54 +0800

    :sparkles: Setup body parser. Change let to const for final variables

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import express
-let express = require('express');
+const express = require('express');
+// Import body parser
+const bodyParser = require("body-parser");
 
 // Initialize the app
-let app = express();
+const app = express();
 
 // Setup server port
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Succesful listening on backend api.'));
+app.get('/', (req, res) => res.send({ message: "Welcome to ILoveZamboanga application." }));
 
 // Launch app to listen to specified port
 app.listen(port, function () {
